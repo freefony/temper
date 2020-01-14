7d5a9d9d7a540b4a9292edfb4430efd60d7e3f62
Femi Oni <freefony@gmail.com>
2020-01-14 12:39:24 +0100

    feat: background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import { Background } from './common/background'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Background />
       </header>
     </div>
   );
