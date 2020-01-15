16a30334b9d75bfa94ff6165ece2eb8288a15929
Femi Oni <freefony@gmail.com>
2020-01-15 12:14:07 +0100

    chore: rename dirs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './styles/tailwind.css'
 
-import { Background } from './common/background'
+import { Background } from './common/background/background'
 import Stage from './stage/Stage'
 
 function App() {
